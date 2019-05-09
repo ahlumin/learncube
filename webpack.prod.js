3b69ea64b800a499938ddc9340023d5f1ab2a7f0
Ahlumin <magus52941@gmail.com>
2019-05-09 17:40:56 +0800

    Releast at v0.1

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,19 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "[name].js?[hash:8]",
+    publicPath: "https://ahlumin.github.io/learncube/"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, "index.html"),
+      template: "index.html"
+    })
+  ]
 });
